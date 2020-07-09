60ff3c0da3cec70dd3ed3078652abb82a9f69f73
supriya <supriyakrishna323@gmail.com>
2020-07-09 19:19:17 +0530

    product page changes

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -2,40 +2,41 @@
   <div>
     <h1>All products</h1>
     <div class="cart">
-      <span>{{ cartProducts.length }}</span>
-      <i
-        class="el-icon-shopping-cart-full
-"
-      ></i>
-      <h2>{{ cartProducts.length }} items in basket</h2>
-      <div class="cart-block">
+      <div class="cart-size">
+        <el-button
+          icon="el-icon-shopping-cart-full"
+          style="background-color:black;color:white"
+          @click="showCartDetails"
+        >{{ cartProducts.length }}</el-button>
+      </div>
+      <el-dialog title="Tips" :visible.sync="dialogVisible" width="30%">
+        <span>This is a message</span>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="dialogVisible = false">Confirm</el-button>
+        </span>
+      </el-dialog>
+      <div class="cart-block" v-if="showCart">
         <ul :key="item.title" v-for="item in cartProducts">
           <li>
-            <img :src="item.image" style="width:50px;height:50px;" />
+            <img :src="item.image" style="width:50px;height:50px;" @click="openDialog" />
             <div class="cart-title">{{ item.title }}</div>
-            <span><i class="el-icon-circle-plus"></i></span>
-            <span>1</span>
-            <span><i class="el-icon-remove"></i></span>
+            <el-button @click="increment(item)" icon="el-icon-circle-plus" circle size="mini"></el-button>
+            <span>{{item.quantity}}</span>
+            <el-button @click="decrement(item)" icon="el-icon-remove" circle size="mini"></el-button>
             <span>{{ item.price }}</span>
           </li>
         </ul>
       </div>
     </div>
     <el-row>
-      <el-col
-        :span="5"
-        v-bind:key="item.id"
-        v-for="item in products"
-        :offset="1"
-      >
+      <el-col :span="5" v-bind:key="item.id" v-for="item in products" :offset="1">
         <el-card :body-style="{ padding: '0px' }">
           <img :src="item.image" class="image" style="width:18em;height:18em" />
           <div style="padding: 14px;">
             <span>{{ item.title }}</span>
             <div class="bottom clearfix">
-              <el-button type="text" class="button" @click="addToCart(item)"
-                >Add to Cart</el-button
-              >
+              <el-button type="text" class="button" @click="addToCart(item)">Add to Cart</el-button>
             </div>
           </div>
         </el-card>
@@ -47,23 +48,54 @@
 <script>
 import axios from "axios";
 export default {
+  name: "Products",
   data() {
     return {
       products: "",
       cartSize: 0,
       cartProducts: [],
+      showCart: false,
+      dialogVisible: false
     };
   },
   mounted() {
-    axios.get("https://fakestoreapi.com/products/").then((response) => {
+    axios.get("https://fakestoreapi.com/products/").then(response => {
       this.products = response.data;
     });
   },
   methods: {
     addToCart(item) {
-      this.cartProducts.push(item);
+      let count = 0;
+      if (!item.quantity) {
+        count = item.quantity ? item.quantity : 0;
+        this.$set(item, "quantity", count + 1);
+        this.cartProducts.push(item);
+      } else {
+        let temp = this.cartProducts.findIndex(p => p.id === item.id);
+        let c = item.quantity + 1;
+        this.$set(this.cartProducts[temp], "quantity", c);
+      }
     },
-  },
+    increment(item) {
+      if (item.quantity == 10) alert("maximum only 10 per items");
+      else item.quantity++;
+    },
+    decrement(item) {
+      if (item.quantity == 1) {
+        let temp = this.cartProducts.findIndex(p => p.id === item.id);
+        this.cartProducts.splice(temp, 1);
+      } else item.quantity--;
+    },
+    showCartDetails() {
+      if (this.cartProducts.length != 0) {
+        this.showCart = !this.showCart;
+      } else this.showCart = false;
+    },
+    openDialog() {
+      console.log("in");
+      this.dialogVisible = true;
+    }
+  }
 };
 </script>
 
